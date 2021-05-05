b883932dee86187c8f56d6bb160e5cbaa9f210ce
Wilfredo R Colina <wrca26@gmail.com>
2021-05-05 12:22:02 -0500

    moving the routing logic to the

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import login from "./Auth/Login";
-import Register from "./Auth/Register";
-import Home from "./Home/Home";
-import "./App.css"
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" exact component={login} />
-        <Route path="/register" exact component={Register} />
-      </Switch>
-    </Router>
-  )
-};
 
+const App = () => {
+
+    return (
+        <h1> App</h1>
+
+    )
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./components/App"
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
+import login from "./components/Auth/Login"
+import Register from "./components/Auth/Register";
+import App from "./components/App";
+import firebase from "./Firebase/firebase";
 
+const Root = (props) => {
 
-ReactDOM.render(
-  <App />,
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log(user);
+        props.history.push("/");
+      }
+    })
+  }, [props.history])
+
+  return (
+
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/login" exact component={login} />
+      <Route path="/register" exact component={Register} />
+    </Switch>
+
+  )
+};
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(<Router><RootWithAuth /> </Router>
+  ,
   document.getElementById('root')
 );
 
